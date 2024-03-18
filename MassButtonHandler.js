03b358a840fb34ce52c02aabe65e753817f03766
jemrules <36177713+jemrules@users.noreply.github.com>
2024-03-18 08:48:25 -0600

    Update MassButtonHandler.js

--- MassButtonHandler.js
+++ MassButtonHandler.js
@@ -1,1 +1,33 @@
+const data = JSON.parse(`
+{
+  "Java" : {
+    "PDE": {"Source":"https://jemrules.github.io/Pong/Pong.pde"}
+  },
+  "Python" : {
+    "PyGame" : {"Source":"https://jemrules.github.io/Pong/Pong.py"}
+  }
+}`);
+document.body.onload = loadSources;
 
+function loadSources() {
+  console.log(data["Java"]);
+  let srcs = document.getElementsByTagName("body")[0];
+  for (var x in data) {
+    let ele = document.createElement("select");
+    // ele.parentElement(document.getElementsByName("sources"))
+    console.log(x);
+    ele.setAttribute("name", x);
+    srcs.appendChild(ele);
+    let op = document.createElement("option");
+    op.textContent = x;
+    ele.appendChild(op);
+    for (var y in data[x]) {
+      let op = document.createElement("option");
+      op.setAttribute("onclick", "window.location.href=this.value;");
+      console.log(data[x][y]["Source"]);
+      op.setAttribute("value", data[x][y]["Source"]);
+      op.textContent = y;
+      ele.appendChild(op);
+    }
+  }
+}
